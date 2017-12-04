b705669b1ca710f2ddc4bd601370466f399185af
onedoorysk <rysk1008@gmail.com>
2017-12-05 02:43:52 +0900

    remove promise middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,9 @@
 import TYPE from './_actionType'
 
-/* Promise action*/
-export const toggleAction = id => new Promise((resolve) => {
-  setTimeout(() => {
-    resolve({type: TYPE.TOGGLE_TODO, payload: {id}})
-  }, 500)
-})
+/* Thunk action*/
+export const toggleAction = id => dispatch => setTimeout(() => {
+  dispatch({type: TYPE.TOGGLE_TODO, payload: {id}})
+}, 500)
 
 /* Thunk action*/
 export const addAction = description => dispatch => setTimeout(() => {
--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
 import rootReducer from './reducers/'
 import {createStore, applyMiddleware} from 'redux'
 import addLoggingToDispatch from 'redux-logger'
-import addPromiseSupportToDispatch from 'redux-promise'
 import thunk from 'redux-thunk'
 
-const middlewares = [thunk, addPromiseSupportToDispatch, addLoggingToDispatch]
+const middlewares = [thunk, addLoggingToDispatch]
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
